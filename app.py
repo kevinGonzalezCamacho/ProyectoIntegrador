f4e58a6a3b47a44412b26c1ddce571c03be91732
ricardofm09 <133308075+ricardofm09@users.noreply.github.com>
2023-07-13 16:17:31 -0600

    proyecto2do

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
@@ -81,6 +82,112 @@ def producto_servicio():
     cursor.close()
 
     return render_template('producto_servicio.html', servicios=servicios, productos=productos)
+
+@app.route('/buscar-producto', methods=['GET'])
+def buscar_producto():
+    if request.method == 'GET':
+        product_name = request.args.get('product_name')
+
+        cursor = mysql.connection.cursor()
+        query = "SELECT * FROM Producto WHERE nombre LIKE %s"
+        values = ('%' + product_name + '%',)
+        cursor.execute(query, values)
+        productos = cursor.fetchall()
+        cursor.close()
+
+        return render_template('producto_servicio.html', servicios=[], productos=productos)
+
+@app.route('/buscar-servicio', methods=['GET'])
+def buscar_servicio():
+    if request.method == 'GET':
+        service_name = request.args.get('service_name')
+        if service_name is None:
+            service_name = ""
+
+        cursor = mysql.connection.cursor()
+        query = "SELECT * FROM Servicios WHERE nombre LIKE %s"
+        values = ('%' + service_name + '%',)
+        cursor.execute(query, values)
+        servicios = cursor.fetchall()
+        cursor.close()
+
+        return render_template('producto_servicio.html', servicios=servicios, productos=[])
+
+@app.route('/agregar-servicio')
+def agregar_servicio():
+    return render_template('agregar_servicio.html')
+
+@app.route('/guardar-servicio', methods=['POST'])
+def guardar_servicio():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+
+        cursor = mysql.connection.cursor()
+        query = "INSERT INTO Servicios (nombre, precio, fecha_inicio, fecha_fin) VALUES (%s, %s, %s, %s)"
+        values = (name, price, start_date, end_date)
+        cursor.execute(query, values)
+        mysql.connection.commit()
+        cursor.close()
+
+        flash('Servicio agregado correctamente')
+
+    return redirect(url_for('producto_servicio'))
+
+@app.route('/agregar-producto')
+def agregar_producto():
+    return render_template('agregar_producto.html')
+
+@app.route('/guardar-producto', methods=['POST'])
+def guardar_producto():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+        stock = request.form['stock']
+
+        cursor = mysql.connection.cursor()
+        query = "INSERT INTO Producto (nombre, precio, descripcion, stock) VALUES (%s, %s, %s, %s)"
+        values = (name, price, description, stock)
+        cursor.execute(query, values)
+        mysql.connection.commit()
+        cursor.close()
+
+        flash('Producto agregado correctamente')
+
+    return redirect(url_for('producto_servicio'))
+
+@app.route('/carrito')
+def carrito():
+    # Aquí debes agregar la lógica para obtener los datos del carrito y calcular el total
+    carrito = [
+        {
+            'producto_id': 1,
+            'nombre': 'Producto 1',
+            'precio': 10.00,
+            'cantidad': 2,
+            'total': 20.00
+        },
+        {
+            'producto_id': 2,
+            'nombre': 'Producto 2',
+            'precio': 15.00,
+            'cantidad': 1,
+            'total': 15.00
+        },
+        {
+            'producto_id': 3,
+            'nombre': 'Producto 3',
+            'precio': 5.00,
+            'cantidad': 4,
+            'total': 20.00
+        }
+    ]
+    total_carrito = 55.00
+
+    return render_template('carrito.html', carrito=carrito, total_carrito=total_carrito)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
